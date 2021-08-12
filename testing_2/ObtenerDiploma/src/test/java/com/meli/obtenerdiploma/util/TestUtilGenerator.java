d4fe463bd596d77d2856b831cb694bd57bd0df11
sebsalazari <sebastian.salazar@mercadolibre.com.co>
2021-08-11 19:20:34 -0500

    [Add] Service test full - Test for ObtenerDiploma y Student services - Tes all passed and coverage 68%

--- testing_2/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/util/TestUtilGenerator.java
+++ testing_2/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/util/TestUtilGenerator.java
@@ -3,6 +3,7 @@ package com.meli.obtenerdiploma.util;
 import com.meli.obtenerdiploma.model.StudentDTO;
 import com.meli.obtenerdiploma.model.SubjectDTO;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -43,7 +44,7 @@ public class TestUtilGenerator {
       return users;
    }
 
-   public static Set<StudentDTO> getOneUser(){
+   public static Set<StudentDTO> getOneUser() {
       Set<StudentDTO> users = new HashSet<>();
 
       StudentDTO student2 = new StudentDTO(2L, "Leito Messi", null, null, new ArrayList<>() {
@@ -57,5 +58,47 @@ public class TestUtilGenerator {
       users.add(student2);
 
       return users;
+   }
+
+   public static StudentDTO getUser() {
+      StudentDTO student2 = new StudentDTO(2L, "Leito Messi", null, null, new ArrayList<>() {
+         {
+            add(new SubjectDTO("Quimica", 10.0));
+            add(new SubjectDTO("Biologia", 8.0));
+            add(new SubjectDTO("Religion", 4.0));
+         }
+      });
+
+      return student2;
+   }
+
+   public static StudentDTO getStudentHonor() {
+      StudentDTO student2 = new StudentDTO(2L, "Leito Messi",
+              "El alumno Leito Messi ha obtenido un promedio de 9.53. Felicitaciones!",
+              (10.0 + 9.5 + 9.1) / 3,
+              new ArrayList<>() {
+                 {
+                    add(new SubjectDTO("Quimica", 10.0));
+                    add(new SubjectDTO("Biologia", 9.5));
+                    add(new SubjectDTO("Religion", 9.1));
+                 }
+              });
+
+      return student2;
+   }
+
+   public static StudentDTO getStudentNotHonor() {
+      StudentDTO student2 = new StudentDTO(2L, "Leito Messi",
+              "El alumno Leito Messi ha obtenido un promedio de 8.53. Puedes mejorar.",
+              (8.0 + 8.5 + 9.1) / 3,
+              new ArrayList<>() {
+                 {
+                    add(new SubjectDTO("Quimica", 8.0));
+                    add(new SubjectDTO("Biologia", 8.5));
+                    add(new SubjectDTO("Religion", 9.1));
+                 }
+              });
+
+      return student2;
    }
 }
