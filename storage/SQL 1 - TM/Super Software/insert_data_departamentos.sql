fe7feb561280e8132b2d20bf8e030b8499a08929
sebsalazari <sebastian.salazar@mercadolibre.com.co>
2021-08-18 14:09:56 -0500

    [Update] sql and add querys of inserts for all tables

---
+++ storage/SQL 1 - TM/Super Software/insert_data_departamentos.sql
@@ -0,0 +1,7 @@
+use emple_dep;
+
+INSERT INTO emple_dep.departamentos (`id_dep`,`nombre_dep`,`direccion_dep`) VALUES (1,'IT','WeWork');
+INSERT INTO emple_dep.departamentos (`id_dep`,`nombre_dep`,`direccion_dep`) VALUES (2,'Costume CX','WeWork');
+INSERT INTO emple_dep.departamentos (`id_dep`,`nombre_dep`,`direccion_dep`) VALUES (3,'People','Ecotower');
+INSERT INTO emple_dep.departamentos (`id_dep`,`nombre_dep`,`direccion_dep`) VALUES (4,'Fintech','Vizcaya');
+INSERT INTO emple_dep.departamentos (`id_dep`,`nombre_dep`,`direccion_dep`) VALUES (5,'RRHH','Ecotower');
